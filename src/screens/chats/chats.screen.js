3607b59ba2cd941064b459a4abd51efba037ebf9
Uurtsaikh <ubatbile@amazon.com>
2018-04-14 00:35:02 -0700

    text input and button

--- src/screens/chats/chats.screen.js
+++ src/screens/chats/chats.screen.js
@@ -1,122 +1,81 @@
-import React, { Component } from "react";
-import { StyleSheet, View, TouchableHighlight } from "react-native";
-
+import React, { Component } from 'react';
 import {
-  Container,
-  Header,
-  Content,
-  List,
-  ListItem,
-  Left,
-  Body,
-  Right,
-  Thumbnail,
-  Text
-} from "native-base";
+    StyleSheet,
+    View,
+    TouchableHighlight,
+    TextInput,
+    Text,
+    Button,
+    TouchableOpacity
+} from 'react-native';
 
-import firebase from "react-native-firebase";
+import firebase from 'react-native-firebase';
 export default class ChatsScreen extends Component {
     state = {
-        data : '',
-        firestore : []
+        data: '',
+        message: ''
+    };
+    static navigationOptions = {
+        title: 'Chats'
+    };
+
+    conversationHandler = () => {
+        console.log('clicked');
+    };
+
+    send = () => {
+        const { message } = this.state;
+
+        alert(message);
+    };
+
+    render() {
+        firebase
+            .database()
+            .ref('test')
+            .on('value', d =>
+                this.setState({
+                    data: d.val()
+                })
+            );
+
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter message"
+                    onChangeText={message => {
+                        this.setState({ message });
+                    }}
+                />
+                <TouchableOpacity style={styles.button} onPress={this.send}>
+                    <Text style={styles.buttonText}>Send</Text>
+                </TouchableOpacity>
+            </View>
+        );
     }
-  static navigationOptions = {
-    title: "Chats"
-  };
-
-  conversationHandler = () => {
-    console.log("clicked");
-  };
-
-  render() {
-    firebase
-      .database()
-      .ref("test")
-      .on("value", d => this.setState({
-          data : d.val()
-      }));
-    return (
-      <Container>
-        <Content>
-          <List>
-            <ListItem
-              avatar
-              button
-              onPress={this.conversationHandler.bind(this)}
-            >
-              <Left>
-                <Thumbnail
-                  source={{
-                    uri: "https://placeimg.com/200/200/people/tech"
-                  }}
-                />
-              </Left>
-              <Body>
-                <Text>Mandal Tsaschikher</Text>
-                <Text note>
-                  Doing what you like will always keep you happy . . {this.state.data}
-                  {JSON.stringify(this.state.firestore)}
-                </Text>
-              </Body>
-              <Right>
-                <Text note>3:43 pm</Text>
-              </Right>
-            </ListItem>
-            <ListItem
-              avatar
-              button
-              onPress={this.conversationHandler.bind(this)}
-            >
-              <Left>
-                <Thumbnail
-                  source={{
-                    uri: "https://placeimg.com/200/200/people"
-                  }}
-                />
-              </Left>
-              <Body>
-                <Text>Luvsansharav Altangerel</Text>
-                <Text note>
-                  Doing what you like will always keep you happy . .
-                </Text>
-              </Body>
-              <Right>
-                <Text note>3:43 pm</Text>
-              </Right>
-            </ListItem>
-            <ListItem
-              avatar
-              button
-              onPress={this.conversationHandler.bind(this)}
-            >
-              <Left>
-                <Thumbnail
-                  source={{
-                    uri: "https://placeimg.com/200/200/animal"
-                  }}
-                />
-              </Left>
-              <Body>
-                <Text>Shagai Nyamdorj</Text>
-                <Text note>
-                  Doing what you like will always keep you happy . .
-                </Text>
-              </Body>
-              <Right>
-                <Text note>3:43 pm</Text>
-              </Right>
-            </ListItem>
-          </List>
-        </Content>
-      </Container>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 10
+    },
+    input: {
+        height: 50,
+        backgroundColor: '#fff',
+        marginBottom: 10,
+        paddingLeft: 15
+    },
+    button: {
+        backgroundColor: 'orange',
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 17
+    }
 });
